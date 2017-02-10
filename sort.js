832ef15ec8d928d0364b596f9bcf271e8d961036
mlxiao <mlxiao93@163.com>
2017-02-10 22:08:09 +0800

    update

--- sort.js
+++ sort.js
@@ -50,9 +50,102 @@ function bubbleSort(arr) {
   return arr;
 }
 
+function shellSort(arr) {
+  var len = arr.length;
+  var h = Math.ceil(len / 3);
+  while(h > 0) {
+    //对间隔为h的元素进行插入排序
+    for (var i = h; i < len; i++) {
+      for (var j = i; j > 0; j -= h) {
+        if (arr[j] < arr[j - h]) {
+          swap(arr, j, j - h);
+        }
+      }
+    }
+    h = Math.floor(h / 3);
+  }
+  return arr;
+}
+
+var aux = []    //归并所需的辅助数组
+
+//原地归并方法
+//arr是一个low到mid和mid+1到high各自有序的数组，借助aux原地merge这个数组
+function merge(arr, low, mid, high) {
+  aux = [];
+  var m = low,
+      n = mid + 1;
+
+  for (var k = low; k <= high; k++) {    //复制arr到aux
+    aux[k] = arr[k];
+  }
+
+  for (var i = low; i <= high; i++) {    //归并aux到arr
+    if (m > mid) {    //左边归并完成
+      arr[i] = aux[n++];
+    } else if (n > high) {  //右边归并完成
+      arr[i] = aux[m++];
+    } else if (aux[m] > aux[n]) {   //左边大于右边
+      arr[i] = aux[n++];
+    } else {       //右边大于左边
+      arr[i] = aux[m++];
+    }
+  }
+}
+
+function mergeSort(arr, low, high) {
+  if (low === undefined) low = 0;
+  if (high === undefined) high = arr.length - 1;
+  if (low >= high) return arr;
+  var mid = Math.floor((low + high) / 2);
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+  merge(arr, low, mid, high);
+  return arr;
+}
+
+//选择一个元素（哨兵）对数组切分，小的放前面，大的放后面（升序）
+//返回切分后哨兵的索引
+function partion(arr, low, high) {
+  var i = low,
+      j = high,
+      k = low,    //切分前哨兵的索引
+      ele = arr[k];
+
+  while(true) {   //扫描左右，检查扫描是否结束
+    while(true) {    //从左边开始扫描，找出比ele大的
+      if(i > j) break;
+      if (arr[i] > ele) break;
+      i++;
+    }
+    while(true) {     //从右边开始扫描，找出比ele小的
+      if(j < i) break;
+      if (arr[j] < ele) break;
+      j--;
+    }
+    if (i >= j) break;   //左边没找到比ele小的或者右边没找到比ele大的
+    swap(arr, i, j); //交换左右找到的元素
+  }
+  k = i - 1;     //切分后哨兵的索引
+  swap(arr, low, k);
+  return k;
+}
+
+function quickSort(arr, low, high) {
+  if (low === undefined) low = 0;
+  if (high === undefined) high = arr.length - 1;
+
+  if (low >= high) return arr;
+
+  var mid =partion(arr, low, high);
+  quickSort(arr, low, mid - 1);
+  quickSort(arr, mid + 1, high);
+
+  return arr;
+}
 
 var arr = generateRandomArray();
 console.log(arr);
-console.log(bubbleSort(arr));
+console.log(quickSort(arr));
 
 export default {};
