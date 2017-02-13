6abd08aaacb195fb555439c439fe85351c32b3d1
mlxiao <mlxiao93@163.com>
2017-02-14 00:20:48 +0800

    update

--- playground.js
+++ playground.js
@@ -4,38 +4,17 @@ function swap(arr, i, j) {
   arr[j] = tmp;
 }
 
-function partion(arr, low, high) {
-  var left = low,
-      right = high,
-      midVal = arr[low];
-
-  while (true) {
-    while (true) {
-      if (left > right) break;
-      if (arr[left] > midVal) break;
-      left++;
+function shellSort(arr) {
+  var len = arr.length;
+  var step = Math.floor(len / 3);
+  while(step > 0) {
+    for (var i = step; i < len; i++) {
+      for (var j = i; j >= step; j -= step) {
+        if (arr[j] < arr[j - step]) swap(arr, j, j - step);
+      }
     }
-    while (true) {
-      if (right < left) break;
-      if (arr[right] < midVal) break;
-      right--;
-    }
-    if (left >= right) break;
-    swap(arr, left, right);
+    step = Math.floor(step / 3);
   }
-  left--;
-  swap(arr, low, left);
-  return left;
-}
-
-function quickSort(arr, low, high) {
-  if (low === undefined) low = 0;
-  if (high === undefined) high = arr.length - 1;
-  if (low >= high) return;
-  var mid = partion(arr, low, high);
-  quickSort(arr, low, mid - 1);
-  quickSort(arr, mid + 1, high);
-  return arr;
 }
 
 var arr = [];
@@ -43,5 +22,5 @@ for (var i = 0; i <= 10; i++) {
   arr.push(Math.ceil(Math.random() * (30 - 10) + 10));
 }
 console.log(arr);
-quickSort(arr);
+shellSort(arr);
 console.log(arr);
